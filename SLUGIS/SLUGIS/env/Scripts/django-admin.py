f1441b3bc012470f5d96584d29be5324268437a0
Dane Low <dnlow@live.com>
2018-01-17 16:23:52 -0800

    django + angular initial commit

---
+++ SLUGIS/SLUGIS/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\SLUGIS\source\repos\SLUGIS\SLUGIS\SLUGIS\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
